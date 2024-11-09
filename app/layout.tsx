d4baccc124e744700b50b4222e05bc372628c347
Drew <hilarioandrew12@gmail.com>
2024-11-09 20:18:38 +0800

    feat: Add meta tags for SEO optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from 'next-themes'
 import { TanstackProvider } from "@/components/TanstackQueryProvider";
+import Head from 'next/head';
+
 
 const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
@@ -18,6 +20,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta name="description" content="Discover the supernatural world of Dandadan, where ghosts and aliens collide in an epic adventure." />
+        <meta name="keywords" content="dandadan, manga, supernatural, ghosts, aliens, adventure" />
+        <meta name="author" content="AINZ" />
+        <meta property="og:title" content="Dandadan Manga" />
+        <meta property="og:description" content="Discover the supernatural world of Dandadan, where ghosts and aliens collide in an epic adventure." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://dandadan-manga.vercel.app/" />
+        <meta property="og:image" content="https://dandadan-manga.vercel.app/og-image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Dandadan Manga" />
+        <meta name="twitter:description" content="Discover the supernatural world of Dandadan, where ghosts and aliens collide in an epic adventure." />
+        <meta name="twitter:image" content="https://dandadan-manga.vercel.app//twitter-image.jpg" />
+      </Head>
+      
       <body className={inter.className}>
       <TanstackProvider>
         <ThemeProvider>
